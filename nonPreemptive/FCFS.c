f1042892774dc5fb9a5698a2ebca21edf0606822
parthpatel2374 <dcpatel577@gmail.com>
2023-04-14 22:42:00 +0530

    Non preemptive folder

---
+++ nonPreemptive/FCFS.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+void swapFloat(float arr[], int upperIndex, int lowerIndex){
+    float temp = arr[lowerIndex];
+    arr[lowerIndex] = arr[upperIndex];
+    arr[upperIndex] = temp;
+}
+void swapInt(int arr[], int upperIndex, int lowerIndex){
+    int temp = arr[lowerIndex];
+    arr[lowerIndex] = arr[upperIndex];
+    arr[upperIndex] = temp;
+}
+void sort_arrivalTime(int proc[], float arival[], float burst[], int numberOfProcess){
+    for(int i=0; i<numberOfProcess; i++){
+        for(int j=0; j<numberOfProcess-1; j++){
+            int index = i;
+            if(arival[j] > arival[i]){
+                swapInt(proc, i, j);
+                swapFloat(arival, i, j);
+                swapFloat(burst, i, j);
+            }
+        }
+    }
+}
+void print_values(int proc[], float arival[], float burst[], float completion[], float waiting[], float turnaround[], int numberOfProcess){
+    printf("ProcNum\tArivalTime\tBurstTime\tCompletionTime\tWaitingTime\tTurnaroundTime\n");
+    for(int i=0; i<numberOfProcess; i++){
+        for(int j=0; j<6; j++){
+            switch(j){
+                case 0:
+                    printf("\n%.1d\t\t", proc[i]);
+                    break;
+                case 1:
+                    printf("%.1f\t\t", arival[i]);
+                    break;
+                case 2:
+                    printf("%.1f\t\t", burst[i]);
+                    break;
+                case 3:
+                    printf("%.1f\t\t", completion[i]);
+                    break;
+                case 4:
+                    printf("%.1f\t\t", waiting[i]);
+                    break;
+                case 5:
+                    printf("%.1f\t\t", turnaround[i]);
+                    break;
+            }
+        }   
+    }
+    printf("\n");
+}
+int main(){
+    int numberOfProcess; 
+    printf("Enter number of processes : ");
+    scanf("%d", &numberOfProcess);
+    
+    float arival[numberOfProcess];
+    float burst[numberOfProcess];
+    float completion[numberOfProcess];
+    float waiting[numberOfProcess]; 
+    float turnaround[numberOfProcess], sumwt=0, sumtt=0, avgwt, avgtt;
+    int proc[numberOfProcess];
+   
+    //input for processes
+    printf("Enter the arrivaltime : ");
+    for(int i=0; i<numberOfProcess; i++){
+        proc[i] = i+1;
+        scanf("%f", &arival[i]);
+    }
+    printf("Enter the bursttime : ");
+    for(int i=0; i<numberOfProcess; i++){
+        scanf("%f", &burst[i]);
+    }
+    
+    //sorting by arival time
+    sort_arrivalTime(proc, arival, burst, numberOfProcess);
+    printf("\n");
+
+    //completion time
+    completion[0] = burst[0];
+    for(int i=1; i<numberOfProcess; i++){
+        if(completion[i-1] < arival[i]){
+            completion[i] = arival[i] + burst[i];
+        }
+        else{
+            completion[i] = completion[i-1] + burst[i];
+        }
+    }
+    
+    //waiting and turnaround calc
+    for(int i=0; i<numberOfProcess; i++){
+        waiting[i] = completion[i] - arival[i] - burst[i];
+        turnaround[i] = completion[i] - arival[i];
+    }
+    for(int i=0; i<numberOfProcess; i++){
+        sumwt = sumwt+waiting[i];
+        sumtt = sumtt+turnaround[i];
+    }
+    avgwt = sumwt/numberOfProcess; avgtt = sumtt/numberOfProcess;
+
+    //printing values
+    print_values(proc, arival, burst, completion, waiting, turnaround, numberOfProcess);
+    printf("AverageWaitingTime = %0.1f, and AverageTurnaroundTime = %0.1f\n", avgwt, avgtt);
+    return 0;
+}
---
+++ nonPreemptive/priorityNP.c
@@ -0,0 +1,144 @@
+#include<stdio.h>
+#include<stdbool.h>
+void swapFloat(float arr[], int upperIndex, int lowerIndex){
+    float temp = arr[lowerIndex];
+    arr[lowerIndex] = arr[upperIndex];
+    arr[upperIndex] = temp;
+}
+void swapInt(int arr[], int upperIndex, int lowerIndex){
+    int temp = arr[lowerIndex];
+    arr[lowerIndex] = arr[upperIndex];
+    arr[upperIndex] = temp;
+}
+void sort_arrivalTime(int proc[], float arival[], float burst[], int priority[], int numberOfProcess){
+    for(int i=0; i<numberOfProcess; i++){
+        for(int j=0; j<numberOfProcess-1; j++){
+            int index = i;
+            if(arival[j] > arival[i]){
+                swapInt(proc, i, j);
+                swapInt(priority, i, j);
+                swapFloat(arival, i, j);
+                swapFloat(burst, i, j);
+            }
+        }
+    }
+}
+void sort_priority(int proc[], float arival[], float burst[], int priority[], int numberOfProcess, int k){
+    for(int i=k; i<numberOfProcess; i++){
+        int index=i;
+        for(int j=i+1; j<numberOfProcess; j++){
+            if(priority[j] < priority[i]){
+                swapInt(proc, i, j);
+                swapInt(priority, i, j);
+                swapFloat(arival, i, j);
+                swapFloat(burst, i, j);
+            }
+        }
+    }
+}
+void print_values(int proc[], float arival[], float burst[], int priority[], float completion[], float waiting[], float turnaround[], int numberOfProcess){
+    printf("ProcNum\tArivalTime\tBurstTime\tPriority\tCompletionTime\tWaitingTime\tTurnaroundTime\n");
+    for(int i=0; i<numberOfProcess; i++){
+        for(int j=0; j<7; j++){
+            switch(j){
+                case 0:
+                    printf("\n%.1d\t\t", proc[i]);
+                    break;
+                case 1:
+                    printf("%.1f\t\t", arival[i]);
+                    break;
+                case 2:
+                    printf("%.1f\t\t", burst[i]);
+                    break;
+                case 3:
+                    printf("%.1d\t\t", priority[i]);
+                    break;
+                case 4:
+                    printf("%.1f\t\t", completion[i]);
+                    break;
+                case 5:
+                    printf("%.1f\t\t", waiting[i]);
+                    break;
+                case 6:
+                    printf("%.1f\t\t", turnaround[i]);
+                    break;
+            }
+        }   
+    }
+    printf("\n");
+}
+int main(){
+    int numberOfProcess; 
+    printf("Enter number of processes : ");
+    scanf("%d", &numberOfProcess);
+    
+    int proc[numberOfProcess], priority[numberOfProcess]; bool ok = true;
+    float arival[numberOfProcess];
+    float burst[numberOfProcess];
+    float completion[numberOfProcess];
+    float waiting[numberOfProcess]; 
+    float turnaround[numberOfProcess], sumwt=0, sumtt=0, avgwt, avgtt;
+   
+    //input for processes
+    printf("Enter the arrivaltime : ");
+    for(int i=0; i<numberOfProcess; i++){
+        proc[i] = i+1;
+        scanf("%f", &arival[i]);
+    }
+    printf("Enter the bursttime : ");
+    for(int i=0; i<numberOfProcess; i++){
+        scanf("%f", &burst[i]);
+    }
+    printf("Enter the priority : ");
+    for(int i=0; i<numberOfProcess; i++){
+        scanf("%d", &priority[i]);
+    }
+
+    //sorting by arival time
+    sort_arrivalTime(proc, arival, burst, priority, numberOfProcess);
+    printf("\n");
+
+    //checking if all processes arive at zero
+    for(int i=0; i<numberOfProcess; i++){
+        if(arival[i] != 0){
+            ok = false;
+        }
+    }
+    
+    //completion calc
+    for(int i=0; i<numberOfProcess; i++){
+        if(ok && i==0){
+            sort_priority(proc, arival, burst, priority, numberOfProcess, i);
+            completion[0] = burst[0];
+        }
+        else{ 
+            if(i == 0){
+                completion[0] = burst[0];
+                continue;
+            }
+            else{    
+                if(completion[i-1] < arival[i]){
+                    completion[i] = arival[i] + burst[i];
+                }
+                if(burst[i-1] > arival[numberOfProcess-1]){
+                    sort_priority(proc, arival, burst, priority, numberOfProcess, i);
+                }
+                completion[i] = completion[i-1] + burst[i];
+            }
+        }
+    }
+
+    //waiting and turnaround calc
+    for(int i=0; i<numberOfProcess; i++){
+        waiting[i] = completion[i] - arival[i] - burst[i];
+        turnaround[i] = completion[i] - arival[i];
+    }
+    for(int i=0; i<numberOfProcess; i++){
+        sumwt = sumwt+waiting[i];
+        sumtt = sumtt+turnaround[i];
+    }
+    avgwt = sumwt/numberOfProcess; avgtt = sumtt/numberOfProcess;
+    print_values(proc, arival, burst, priority, completion, waiting, turnaround, numberOfProcess);
+    printf("AverageWaitingTime = %0.1f, and AverageTurnaroundTime = %0.1f\n", avgwt, avgtt);
+    return 0;
+}
